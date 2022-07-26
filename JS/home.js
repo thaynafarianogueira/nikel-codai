e9bfcb04505e3b4ddbd39c726d6b63e0f0e46826
Thayna Faria Nogueira <thayna.faria@gmail.com>
2022-07-26 10:10:34 -0300

    mudanças

---
+++ JS/home.js
@@ -0,0 +1,71 @@
+const myModal = new bootstrap.Modal("#transaction-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+var data = {
+    transactions:[]
+};
+
+     
+checkLogged();
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+                                                //verificação de login
+    if(!logged) {
+        window.location.href = 'index.html';
+        return;
+    }                                         
+
+    const dataUser = localStorage.getItem(logged);     
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    }
+ 
+}
+
+//botão sair
+
+document.getElementById("button-logout").addEventListener("click", logout);
+
+
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = 'index.html';
+}
+
+   
+    //lançamentos
+
+    document.getElementById("transaction-form") .addEventListener("submit", function(e) {
+             e.preventDefault();
+
+            const value = parseFloat(document.getElementById("value-input").value);
+            const description = document.getElementById("description-input").value;
+            const date = document.getElementById("date-input").value;
+            const type = document.querySelector('input[name="type-input"]:checked').value;
+                
+            data.transactions .unshift({
+                value:value, description: description, date: date, type: type 
+
+
+            });
+
+            
+            saveData();
+            e.target.reset();
+            myModal.hide();
+            
+
+            alert("Lançamento adicionado com sucesso.");
+
+    })
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,15 @@
-//criar conta
+const myModal= new bootstrap.Modal("#registrar-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+                //criar conta
+                  
 document.getElementById("criar-modal").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const email = document.getElementById("email-criar-input").Value;
+    const email = document.getElementById("email-criar-input").value;
     const password = document.getElementById("criar-senha-input").value;
 
     if (email.length < 5) {
@@ -18,14 +25,55 @@ document.getElementById("criar-modal").addEventListener("submit", function (e) {
         password: password,
         transction: []
     })
+   
     myModal.hide();
 
     alert("Conta criada com sucesso");
 });
 
+
 function saveAccount(data) {
     localStorage.setItem(data.login, JSON.stringify(data));
 }
+         
+                 //entrar no sistema
+
+document.getElementById("login-login").addEventListener("submit" , function(e){
+         e.preventDefault();
+
+         const email = document.getElementById("email-input").value;
+         const password = document.getElementById("senha-input").value;
+         const sessioncheck = document.getElementById("session-check").checked;
+         
+         const account = getaAccount(email);
+
+        if(!account){
+            alert("Oops!Verifique o usuário ou a senha ");
+            return;
+        }
+
+        if(account){
+            if(account.password !== password){
+                alert("Oops!Verifique o usuário ou a senha ");
+            return;
+            }
+            
+           SaveSession(email,sessioncheck);
+
+            window.location.href = "home.html";
+        }
+  
+
+});
+
+function SaveSession (data, SaveSession){
+    if(SaveSession){
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
 
 function getaAccount(key) {
     const account = localStorage.getItem(key);
@@ -34,4 +82,16 @@ function getaAccount(key) {
         return JSON.parse(account);
     }
     return "";
+}
+
+
+function checkLogged(){
+    if(session){
+       sessionStorage.setItem("logged", session);
+       logged = session;
+    }
+    if(logged){
+       SaveSession(logged, session);
+       window.location.href = "home.html";
+    }
 }
